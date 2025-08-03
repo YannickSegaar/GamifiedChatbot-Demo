c17724ade9e27103cafaadc5c1af834cad94423c
Yannick Romain Segaar <yrs@Yannicks-MacBook-Pro.local>
2025-08-03 11:21:20 +0200

    Added extensions

---
+++ config.js
@@ -0,0 +1,13 @@
+// Configuration file for API keys and sensitive data
+// This file should never be committed to version control
+
+export const config = {
+  googleMaps: {
+    apiKey: 'AIzaSyAznJHiZA8ZYZQjr7wD32r-wZND-4JnNJQ'
+  },
+  // Add other API keys or sensitive configuration here
+  // Example:
+  // voiceflow: {
+  //   apiKey: 'YOUR_VOICEFLOW_API_KEY'
+  // }
+};
